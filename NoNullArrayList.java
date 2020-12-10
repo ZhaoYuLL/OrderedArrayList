86ba52c1c47076f1084d855cc8e54fd7d069a582
Zhao Yu Lin <>
2020-12-10 12:11:29 -0500

    serializable class has no definition of serialVersionUID error

---
+++ NoNullArrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class NoNullArrayList<T> extends ArrayList<T>{
+  public boolean add(T e){
+    
+  }
+  public boolean add(){
+
+  }
+  public T set(int index, T value){
+
+  }
+
+}
