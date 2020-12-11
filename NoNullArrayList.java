6a99c11a76f296448243840518ba9a5b7c790b92
Zhao Yu Lin <>
2020-12-11 18:54:03 -0500

    added add() with the boolean return type

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -4,12 +4,20 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   public void add(int index, T element){
     try{
       if(element==null){
-        throw new IllegalArgumentException("IS NULL!");
+        throw new IllegalArgumentException("Your element is null");
       }
       super.add(index,element);
     }catch(IllegalArgumentException e){
-      throw new IllegalArgumentException("IS NULL!");
+      throw new IllegalArgumentException("Your element is null");
     }
+  }
+
+  public boolean add(T element){
+    if (element == null) {
+      throw new IllegalArgumentException("Your element is null");
+    }
+    super.add(element);
+    return true;
   }
 
 }
