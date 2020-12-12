81db495794b98bfabef9eb36779004b573e20c4d
Zhao Yu Lin <>
2020-12-12 17:40:47 -0500

    tested set and add for a bit w/ different values

--- Tester.java
+++ Tester.java
@@ -17,12 +17,11 @@ public class Tester {
     ArrayList<Integer> y = new OrderedArrayList<Integer>();
     y.add(4);
     y.add(5);
-    y.add(3);
-    y.add(2);
-    System.out.println("Before setting: " + y);
-    // System.out.println("What did I remove: " + y.set(1,3));
-    // System.out.println("After setting at index = 1: " + y);
-    // System.out.println("What did I remove: " + y.set(2,2));
-    // System.out.println("After setting at index = 2: " + y);
+    y.add(-5);
+    y.add(5);
+    System.out.println("Before: "+ y);
+    y.add(1, 2);
+    y.set(3, 100);
+    System.out.println("After vod add() and set(): " +y);
   }
 }
