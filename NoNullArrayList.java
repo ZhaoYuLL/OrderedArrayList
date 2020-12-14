88fa445c559dbc04a826bb27a69f1e85ef68c0d8
Zhao Yu Lin <>
2020-12-14 00:07:49 -0500

    Fixed the methods(mainly set) so that it worked on Reng's Tester

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,34 +1,33 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
 public class NoNullArrayList<T> extends ArrayList<T>{
-
   public void add(int index, T element){
-    try{
-      if(element==null){
+      try{
+        if(element==null){
+          throw new IllegalArgumentException("Your element is null");
+        }
+        super.add(index,element);
+      }
+      catch(IllegalArgumentException e){
         throw new IllegalArgumentException("Your element is null");
       }
-      super.add(index,element);
-    }catch(IllegalArgumentException e){
-      throw new IllegalArgumentException("Your element is null");
     }
-  }
-
-  public boolean add(T element){
-    if (element == null) {
-      throw new IllegalArgumentException("Your element is null");
+    public boolean add(T element){
+      if (element == null) {
+        throw new IllegalArgumentException("Your element is null");
+      }
+      super.add(element);
+      return true;
     }
-    super.add(element);
-    return true;
-  }
-  public T set(int index, T element) {
+    public T set (int index, T element) {
     if (element == null) {
       throw new IllegalArgumentException("Your element is null");
     }
     T temp = super.get(index);
-    super.add(index,element);
-    super.remove(temp);
+    super.remove(index);
+    super.add(index, element);
     return temp;
   }
-
   public NoNullArrayList() {
     super();
   }
